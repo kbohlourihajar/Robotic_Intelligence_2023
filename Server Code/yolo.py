2d60e1323fb8ef92bc0625d6071b7473f4391e86
Kelton Chenworth <116692617+ancient1341@users.noreply.github.com>
2023-12-06 09:57:25 -0700

    Update yolo.py

--- Server Code/yolo.py
+++ Server Code/yolo.py
@@ -2,8 +2,33 @@ from ultralytics import YOLO
 import cv2
 import time
 
-model = YOLO("C:/Users/kpche/OneDrive/Desktop/cs5510/runs/detect/train8/weights/best.onnx")
+model = YOLO("best.onnx")
 
+def yoloView(frame):
+    items = []
+    results = model.predict(frame)
+    if not results: # check if nothing is seen
+        return items
+    for result in results: #loop through every seen item
+        items.append(getPos(result))
+    
+    return items
+
+def getPos(result)
+    x = result.boxes.xywh[0][0].item()
+    y = result.boxes.xywh[0][1].item()
+    w = result.boxes.xywh[0][2].item()
+    h = result.boxes.xywh[0][3].item()
+    if(print(result.boxes.cls[0].item()) = 0):
+        return {'size': (w+h)/2 'x': x+w/2, 'y': y+h/2, 'type': 'blueBall'}
+    if(print(result.boxes.cls[1].item()) = 0):
+        return {'size': (w+h)/2 'x': x+w/2, 'y': y+h/2, 'type': 'chessBoard'}
+    if(print(result.boxes.cls[2].item()) = 0):
+        return {'size': (w+h)/2 'x': x+w/2, 'y': y+h/2, 'type': 'yellowBall'}
+    else:
+        return {'size': -1 'x': -1, 'y': -1, 'type': 'none'}
+        
+'''
 # Open the webcam
 cap = cv2.VideoCapture(0)
 
@@ -28,30 +53,6 @@ while True:
 # Release the webcam and destroy all windows
 cap.release()
 cv2.destroyAllWindows()
-
-'''
-def yoloView(frame):
-    items = []
-    results = model.predict(frame)
-    for result in results:
-        if(result)
-        items.append(getPos(result))
-    
-    return items
-
-def getPos(result)
-    x = result.boxes.xywh[0][0].item()
-    y = result.boxes.xywh[0][1].item()
-    w = result.boxes.xywh[0][2].item()
-    h = result.boxes.xywh[0][3].item()
-    if(print(result.boxes.cls[0].item()) = 0):
-        return {'size': (w+h)/2 'x': x+w/2, 'y': y+h/2, 'type': 'blueBall'}
-    if(print(result.boxes.cls[1].item()) = 0):
-        return {'size': (w+h)/2 'x': x+w/2, 'y': y+h/2, 'type': 'chessBoard'}
-    if(print(result.boxes.cls[2].item()) = 0):
-        return {'size': (w+h)/2 'x': x+w/2, 'y': y+h/2, 'type': 'yellowBall'}
-    else:
-        return {'size': -1 'x': -1, 'y': -1, 'type': 'none'}
 '''
 
 '''
